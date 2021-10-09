99b184331bd0f39bf7c3f759bfb2c5ea387e3d6d
Rence Abishek <renceabishek@gmail.com>
2021-10-09 19:20:31 +0530

    firstcommit

---
+++ src/main/java/com/stjohns/stjohnsyouthservice/StjohnsYouthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stjohns.stjohnsyouthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StjohnsYouthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StjohnsYouthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stjohns/stjohnsyouthservice/StjohnsYouthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stjohns.stjohnsyouthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StjohnsYouthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
